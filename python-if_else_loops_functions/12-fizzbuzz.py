2116b078bd2a8a92066ed73a7a6bc2839a37a0f0
charlybarrio <5757@holbertonstudents.com>
2023-01-19 08:27:55 -0800

    task12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
 
     for i in range(1, 101):
-        elif (i % 3 == 0 and i % 5 == 0):
+        if (i % 3 == 0 and i % 5 == 0):
             print("FizzBuzz".format(i), end=" ")
         elif (i % 3 == 0):
             print("Fizz".format(i), end=" ")
