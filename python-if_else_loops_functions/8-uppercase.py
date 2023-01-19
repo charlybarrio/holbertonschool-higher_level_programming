6360e1148733189fa8ff7cfaab67fe483c5ee8dd
charlybarrio <5757@holbertonstudents.com>
2023-01-19 06:03:44 -0800

    task8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+   
+    Ascii = ""
+    strMayus = ""
+    Strlrg = len(str)
+
+    for n in range(Strlrg):
+        Ascii = ord(str[n])
+        if (97 <= Ascii <= 122):
+            Ascii = Ascii - 32
+        strMayus += chr(Ascii)
+
+    print("{}".format(strMayus))
