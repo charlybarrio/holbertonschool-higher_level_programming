5ab3f90b59e17f5d18f5a8ab3754d3db541dc917
charlybarrio <5757@holbertonstudents.com>
2023-01-22 11:25:48 -0800

    task3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,12 +2,11 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
-    from add_0 import add
     arg = len(argv)
     if (arg == 0):
         print("0")
     else:
         c = 0
         for a in range(1, arg):
-            c += add(int(argv[a - 1]), int(argv[a]))
+            c += int(argv[a - 1]) + int(argv[a])
         print("{}".format(c))
