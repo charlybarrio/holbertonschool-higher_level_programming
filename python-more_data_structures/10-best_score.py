5f222a90d9a8f4cc79ffd676eba05117b723ffd2
charlybarrio <5757@holbertonstudents.com>
2023-01-28 18:22:57 -0800

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-     if a_dictionary is None:
-         return None
-     new_val = max(a_dictionary, key= lambda x: a_dictionary[x])
-     return new_val
+    if a_dictionary is None:
+        return None
+    new_val = max(a_dictionary, key=lambda x: a_dictionary[x])
+    return new_val
