eede51b567debbcafcfd453c7947a7f2b6aeb641
charlybarrio <5757@holbertonstudents.com>
2023-01-29 08:26:15 -0800

    task2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
-    try:
-        for a in range(x):
-            print("{:d}".format(a), end="")
+    for a in range(x):
+        try:
+            print("{:d}".format(my_list[a]), end="")
             n += 1
     except:
         pass
+    print()
     return n
