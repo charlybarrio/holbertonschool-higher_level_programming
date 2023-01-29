6705a0069e04f994e5635e183f654a66dc919b72
charlybarrio <5757@holbertonstudents.com>
2023-01-29 06:59:01 -0800

    task12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3                                                         
+def roman_to_int(roman_string):                                            
+    if type(roman_string) != str and type(roman_string) != type(None):     
+        return 0                                                           
+    else:                                                                  
+        letter_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    value = 0
+    for x in range(len(roman_string)):
+        a = roman_string[x]
+        b = roman_string[x - 1]
+        if x > 0 and letter_val[a] > letter_val[b]:
+            value += letter_val[a] - 2 * letter_val[b]
+        else:
+            value += letter_val[a]
+    return value
