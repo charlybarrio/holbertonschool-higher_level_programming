2f3695c88ea3f96ca92ed53aa9790c5b623dd8d4
charlybarrio <5757@holbertonstudents.com>
2023-01-29 07:04:16 -0800

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,16 @@
-#!/usr/bin/python3                                                         
-def roman_to_int(roman_string):                                            
-    if type(roman_string) != str and type(roman_string) != type(None):     
-        return 0                                                           
-    else:                                                                  
-        letter_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, (str, type(None))):
+        return 0
+    else:
+        letter_val = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000}
     value = 0
     for x in range(len(roman_string)):
         a = roman_string[x]
