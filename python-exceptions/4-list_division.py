ee606d905348bce42504451fdc724e5006332976
charlybarrio <5757@holbertonstudents.com>
2023-01-29 13:48:20 -0800

    task4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        val = 0
+        try:
+            val = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(val)
+    return new_list
