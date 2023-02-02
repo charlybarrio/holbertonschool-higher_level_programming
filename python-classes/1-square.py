aa8d2077014e0080b1f424450c67982f9c64918c
charlybarrio <5757@holbertonstudents.com>
2023-02-01 18:39:51 -0800

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class that defines a square"""
+
+class Square:
+    """ define a square object"""
+    def __init__(self, size):
+        """initializes the square class with a size"""
+        self.__size = size
