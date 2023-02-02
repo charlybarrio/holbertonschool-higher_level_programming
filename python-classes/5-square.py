09615512850a99665b6018bffa76ae531fed53be
charlybarrio <5757@holbertonstudents.com>
2023-02-02 06:50:39 -0800

    task5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -41,7 +41,7 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print ()
+            print()
         else:
             for i in range(self.__size):
                 print("#" * self.__size)
