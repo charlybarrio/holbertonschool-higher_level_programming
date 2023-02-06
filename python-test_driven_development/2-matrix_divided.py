917036ea915836c33ee047c433607822f220f717
charlybarrio <5757@holbertonstudents.com>
2023-02-05 16:11:53 -0800

    task2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""function that divides a matrix"""
+"""
+dev a matrix
+"""
 
 
 def matrix_divided(matrix, div):
-    """Inizializes"""
-    if type(div) not in [int, float]:
+    """
+    div a matrix
+    """
+    if type(matrix) != list or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if len(set(len(row) for row in matrix)) != 1:
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    row_len = len(matrix[0])
-    for row in matrix:
-        if len(row) != row_len:
-            raise TypeError("Each row of the matrix must have the same size")
-        for element in row:
-            msj = "matrix must be a matrix (list of lists) of integers/floats"
-            if type(element) not in [int, float]:
-                raise TypeError(msj)
-
     return [[round(element / div, 2) for element in row] for row in matrix]
+
