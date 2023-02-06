9ca675d14a0a7fc6b21ae3125857e182b13ca25f
charlybarrio <5757@holbertonstudents.com>
2023-02-05 16:37:29 -0800

    task5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,8 +8,16 @@ def text_indentation(text):
     """
     Inizializes
     """
-    if not isinstance(text, str):
+   if not isinstance(text, str):
         raise TypeError("text must be a string")
-    new = text.replace(".", ".\n\n").replace("?", "?\n\n").\
-        replace(":", ":\n\n")
-    print(new_text, end="")
+    bool = False
+    for i in range(len(text)):
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print(text[i] + '\n')
+            bool = True
+        if bool:
+            if text[i + 1] == " ":
+                continue
+            bool = False
+        else:
+            print(text[i], end="") 
