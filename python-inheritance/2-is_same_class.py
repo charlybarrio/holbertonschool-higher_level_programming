590ece31d5e7f12ef6425d71ccd66768239a9c0c
charlybarrio <5757@holbertonstudents.com>
2023-02-09 08:45:37 -0800

    task2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,3 +7,6 @@ Return true if the obj is exactly an instance of the class, if not False
 def is_same_class(obj, a_class):
     """compare obj and instance"""
     return isinstance(obj, a_class)
+
+is_same_class(1, int)    # True
+is_same_class(1, object) # Fale
