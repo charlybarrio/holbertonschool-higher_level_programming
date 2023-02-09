892e2f845c74b773b3387163db068a8df7965868
charlybarrio <5757@holbertonstudents.com>
2023-02-09 06:07:34 -0800

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,9 +3,14 @@
 Write a class MyList that inherits from list
 """
 
+class MyList(list):
+    """
+    Class
+    """
 
-def print_sorted(self):
-    """
-    print list
-    """
-    print sorted(self)
+    def print_sorted(self):
+        """
+        print list
+        """
+        list = sorted(self)
+        print(list)
