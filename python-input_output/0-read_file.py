c9a0dae8fb7bb39e8b1e2088e02d4585ca378ed9
charlybarrio <5757@holbertonstudents.com>
2023-02-11 08:44:12 -0800

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ Read a text file and prints it
 
 def read_file(filename=""):
     """Function"""
-    with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read)
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
