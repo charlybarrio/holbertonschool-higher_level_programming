7119630ea5e95775479b2c9517845f3ac3ef1a5f
charlybarrio <5757@holbertonstudents.com>
2023-02-12 08:09:36 -0800

    task1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,4 +7,5 @@ Writes a string to a text file, return num of char
 def write_file(filename="", text=""):
     """Function"""
     with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
         return len(text)
