2b573540ba625ae9a187a0dd3f7663d8df927e0e
charlybarrio <5757@holbertonstudents.com>
2023-02-12 08:03:27 -0800

    task1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Writes a string to a text file, return num of char
+"""
+
+
+def write_file(filename="", text=""):
+    """Function"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return len(text)
