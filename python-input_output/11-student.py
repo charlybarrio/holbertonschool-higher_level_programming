6950b44a8cde0b262ef6bfc713ed2514977dc8c8
charlybarrio <5757@holbertonstudents.com>
2023-02-12 14:30:22 -0800

    task11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Write a class Student
+"""
+
+
+class Student:
+    """class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            # k = key
+            # v = value
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
