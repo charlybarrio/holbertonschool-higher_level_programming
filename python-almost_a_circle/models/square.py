49c751dcc82fc36d4774f20b682cea5c88cf1cad
charlybarrio <charlybarrio@github.com>
2023-02-14 16:38:11 -0800

    task10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,3 +13,10 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Init"""
         super().__init__(size, size, x, y, id)
+
+    def __strs__(self):
+        id = self.id
+        s = self.__size
+        x = self.__x
+        y = self.__y
+        return ("[Square] ({}) {}/{} - {}".format(id, x, y, s))
