a488593ac9910ac58a034e821e15816a3957f333
charlybarrio <charlybarrio@github.com>
2023-02-14 16:11:49 -0800

    task10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Class Square inherits from Rectangle
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Init"""
+        super().__init__(size, size, x, y, id)
