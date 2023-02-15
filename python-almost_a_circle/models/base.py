21217fa61dfc49e594ed669baf934ac205cc2820
charlybarrio <charlybarrio@github.com>
2023-02-15 07:18:01 -0800

    task16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ Class base
 """
 
 
+import json
+
+
 class Base:
     """class"""
     __nb_objects = 0
@@ -15,3 +18,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of obj to a file"""
+        filename = cls.__name__ + ".json"
+        result = []
+        if list_objs is not None:
+            for atribute in list_objs:
+                result.append(element.to_dictionary())
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(cls.to_json_string(result))
