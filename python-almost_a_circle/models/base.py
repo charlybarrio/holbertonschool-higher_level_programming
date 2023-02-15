30cd99d1ec99422dbf34cb6c8bd96345b3059d43
charlybarrio <charlybarrio@github.com>
2023-02-15 08:07:13 -0800

    task15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,3 +18,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Json representation of list_dictiones"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
