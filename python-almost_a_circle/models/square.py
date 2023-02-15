d73c3141c7b0fd7689f52f365b308422d3c3c812
charlybarrio <charlybarrio@github.com>
2023-02-14 16:49:25 -0800

    task10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,9 +14,20 @@ class Square(Rectangle):
         """Init"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
+
     def __strs__(self):
         id = self.id
-        s = self.__size
-        x = self.__x
-        y = self.__y
+        s = self.width
+        x = self.x
+        y = self.y
         return ("[Square] ({}) {}/{} - {}".format(id, x, y, s))
