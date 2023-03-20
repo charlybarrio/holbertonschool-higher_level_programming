a0ab57253d840effcbfcb104ea6d3d32f95350c7
charlybarrio <5757@holbertonstudents.com>
2023-03-19 17:52:37 -0700

    task4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     # Exectute the query
     cur = db.cursor()
 
-    cur.execute("""SELECT cities.id, cities.name, states.name 
+    cur.execute("""SELECT cities.id, cities.name, states.name
                 FROM cities LEFT JOIN states
                 ON cities.state_id = states.id
                 ORDER BY cities.id ASC""")
