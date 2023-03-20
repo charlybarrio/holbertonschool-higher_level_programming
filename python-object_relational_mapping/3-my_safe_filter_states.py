1e33067529e9844bc0db967482eb3a492937d5f6
charlybarrio <5757@holbertonstudents.com>
2023-03-19 17:33:14 -0700

    task3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+displays all values in the states where name matches the argument
+And is safe from MySQL injections!
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_password,
+            db=database_name)
+
+    # Exectute the query
+    cur = db.cursor()
+
+    query = ("""SELECT * FROM states
+            WHERE name LIKE BINARY %s
+            ORDER BY states.id ASC""", (state_name))
+    cur.execute(query)
+
+    # fetchall statement select the data of the tables
+    records = cur.fetchall()
+
+    for element in records:
+        print(element)
