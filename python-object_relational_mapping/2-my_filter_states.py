0afdeb0ab667bd8e1441d3a6b21d9b7a0bdc618e
charlybarrio <5757@holbertonstudents.com>
2023-03-19 17:08:07 -0700

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+displays all values in the states table where name matches the argument.
 """
 
 if __name__ == "__main__":
