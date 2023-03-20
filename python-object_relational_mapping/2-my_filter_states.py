77c73dd7c2c38b254cbb103b3071fecc6cf8a282
charlybarrio <5757@holbertonstudents.com>
2023-03-19 17:07:18 -0700

    Task2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_password,
+            db=database_name)
+
+    # Exectute the query
+    cur = db.cursor()
+
+    query = ("""SELECT * FROM states
+            WHERE name LIKE BINARY '{}'
+            ORDER BY states.id ASC.format(state_name)""")
+    cur.execute(query)
+
+    # fetchall statement select the data of the tables
+    records = cur.fetchall()
+
+    for element in records:
+        print(element)
