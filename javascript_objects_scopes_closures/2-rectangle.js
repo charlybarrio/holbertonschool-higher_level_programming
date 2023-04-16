23e9ac9edb87671d71fe4a8b5058110651907222
charlybarrio <5757@holbertonstudents.com>
2023-04-16 15:53:46 -0700

    task2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+      return;
+    }
+
+    this.width = w;
+    his.height = h;
+  }
+}
+module.exports = Rectangle;
