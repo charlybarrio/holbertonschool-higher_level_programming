0f025ca746cf2ed136d88432a62837cb98fcafdd
charlybarrio <5757@holbertonstudents.com>
2023-04-16 15:57:19 -0700

    task3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      return;
+    }
+
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
