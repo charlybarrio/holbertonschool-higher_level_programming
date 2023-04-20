30ecbbd1b3466458254a768b4d1bb0dc3fecfc72
charlybarrio <5757@holbertonstudents.com>
2023-04-20 10:58:10 -0700

    task4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const Url = process.argv[2];
+
+request.get(Url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  const filmsWithWedge = data.results.filter(film =>
+    film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')
+  );
+
+  console.log(filmsWithWedge.length);
+});
