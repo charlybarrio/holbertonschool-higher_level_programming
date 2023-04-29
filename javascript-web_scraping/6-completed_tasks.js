ec3bca6ef8e2b4f91e332ad64e6f1f5fa8e149b1
charlybarrio <5757@holbertonstudents.com>
2023-04-29 16:07:14 -0700

    fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,19 +3,23 @@
 const request = require('request');
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-request.get({ url: url }, (err, res, body) => {
+request(url, (err, res, body) => {
   if (err) {
     console.error(err);
-  } else {
-    const todos = JSON.parse(body);
-    const completedUser = {};
-
-    todos.forEach(todo => {
-      if (todo.completed) {
-        const userId = todo.userId;
-        completedUser[userId] = (completedUser[userId] || 0) + 1;
-      }
-    });
-    console.log(completedUser);
+    return;
   }
+
+  const todos = JSON.parse(body);
+  const completedUser = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedUser[todo.userId]) {
+        completedUser[todo.userId]++;
+      } else {
+        completedUser[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(completedUser);
 });
